37ae2951e465ec778ec02c2acc975305bc859cfe
MohdKhal <100966005+MohdKhal@users.noreply.github.com>
2026-02-13 00:10:20 +0530

    Modify EC2 instance settings and add Jenkins user data
    Updated AWS EC2 instance configuration and added user data for Jenkins installation.

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,3 +1,7 @@
+provider "aws" {
+  region = var.aws_region
+}
+
 locals {
   instance_names = [
     "jenkins-server",
@@ -8,19 +12,33 @@ locals {
 }
 
 resource "aws_instance" "ec2" {
-  count                  = var.ec2-instance-count
-  ami                    = data.aws_ami.ubuntu.id
-  subnet_id              = aws_subnet.public-subnet[count.index].id
-  instance_type          = var.ec2_instance_type[count.index]
-  iam_instance_profile   = aws_iam_instance_profile.iam-instance-profile.name
-  vpc_security_group_ids = [aws_security_group.default-ec2-sg.id]
+  count = var.ec2_instance_count
+
+  ami           = data.aws_ami.ubuntu.id
+  subnet_id     = aws_subnet.public_subnet[count.index].id
+  instance_type = var.ec2_instance_type[count.index]
+
+  iam_instance_profile   = aws_iam_instance_profile.iam_instance_profile.name
+  vpc_security_group_ids = [aws_security_group.default_ec2_sg.id]
+
   root_block_device {
     volume_size = var.ec2_volume_size
     volume_type = var.ec2_volume_type
   }
 
+  user_data = count.index == 0 ? <<-EOF
+              #!/bin/bash
+              apt update -y
+              apt install -y openjdk-17-jdk wget
+
+              wget -O /home/ubuntu/jenkins.war https://get.jenkins.io/war-stable/latest/jenkins.war
+              chown ubuntu:ubuntu /home/ubuntu/jenkins.war
+
+              nohup java -Xms256m -Xmx512m -jar /home/ubuntu/jenkins.war --httpPort=8080 > /home/ubuntu/jenkins.log 2>&1 &
+              EOF
+              : null
+
   tags = {
     Name = "${local.org}-${local.project}-${local.env}-${local.instance_names[count.index]}"
-    Env  = "${local.env}"
   }
 }
